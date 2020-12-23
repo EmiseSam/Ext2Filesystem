55d1d98f577041144787c231a300917fd6bf451b
EmiseSam <13710664546@163.com>
2020-12-23 18:00:59 +0800

    add commend

--- filesystem.h
+++ filesystem.h
@@ -6,6 +6,25 @@
 
 struct inode inode[INODE_AMOUNT];
 
+/**
+ * @description:  初始化文件系统
+ * @param {*}
+ * @return {returns 1 on success, 0 otherwise}
+ */
+int init_system();
 
+/**
+ * @description:  打开文件系统
+ * @param {*}
+ * @return {returns 1 on success, 0 otherwise}
+ */
+int open_system();
+
+/**
+ * @description:  关闭文件系统
+ * @param {*}
+ * @return {returns 1 on success, 0 otherwise}
+ */
+int close_system();
 
 #endif
